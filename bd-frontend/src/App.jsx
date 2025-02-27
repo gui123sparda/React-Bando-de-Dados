cab76f9ce3d785392768e557a57bce425b4bb522
gui123sparda <ribeiroguilherme72@gmail.com>
2025-02-27 19:27:08 -0300

    React Postgres Project

---
+++ bd-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    < >
+    <div data-theme="luxury">
+    <Navbar/>
+    </div>
+      
+      
+    </>
+  )
+}
+
+export default App
